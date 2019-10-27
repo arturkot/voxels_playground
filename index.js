9021ac000b3fb3097b41620eed8befe028cd6626
arturkot <arturkot85@gmail.com>
2019-10-27 22:25:55 +0100

    Load and display gltf model

--- index.js
+++ index.js
@@ -3,39 +3,69 @@ import {
     PerspectiveCamera,
     WebGLRenderer,
     BoxGeometry,
-    MeshBasicMaterial,
-    Mesh
-} from "./es_modules/three.js";
+    MeshLambertMaterial,
+    DirectionalLight,
+    AmbientLight,
+    Mesh,
+    VertexColors
+} from "https://unpkg.com/three@0.109.0/build/three.module.js";
+import { GLTFLoader } from "https://unpkg.com/three@0.109.0/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://unpkg.com/three@0.109.0/examples/jsm/controls/OrbitControls.js";
 
 const { innerWidth, innerHeight } = window;
 const scene = new Scene();
 const camera = new PerspectiveCamera(
-    75,
+    45,
     innerWidth / innerHeight,
     0.1,
     1000
 );
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({
+    antialias: true
+});
 
 renderer.setSize(
     innerWidth,
     innerHeight
 );
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
+loader.load("3dExports/corridor_test.gltf", gltf => {
+    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    
+    gltf.scene.traverse(object3d => {
+        const { material } = object3d;
 
-camera.position.z = 5;
+        if (object3d.isMesh && material) {
+            object3d.material = new MeshLambertMaterial({
+                vertexColors: VertexColors
+            });
+            material.dispose();
+        }
+    });
+    
+    scene.add(gltf.scene);
+    
+    const directionalLight = new DirectionalLight(0xffffff, 0.6);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+    
+    const ambientLight = new AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+    
+    new OrbitControls(camera, renderer.domElement);
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.02;
-}
-animate();
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+});
+
+const CAMERA_POSITION = 5;
+camera.position.x = CAMERA_POSITION;
+camera.position.y = CAMERA_POSITION;
+camera.position.z = CAMERA_POSITION;
 
 document.body.appendChild(renderer.domElement);
 renderer.domElement.addEventListener("touchmove", event => {
